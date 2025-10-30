433e33edb48909614d5afada6be9e2108538d2e3
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2025-10-31 00:06:05 +0100

    Wokring with enums

---
+++ projects/enumeration/src/main.rs
@@ -0,0 +1,72 @@
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    
+    let coin1 = Coin::Penny;
+    let coin2 = Coin::Quarter(UsState::Alaska);
+
+    println!("Value of coin1: {} cents", value_in_cent(coin1));
+    println!("Value of coin2: {} cents", value_in_cent(coin2));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("six: {:?}", six);
+    println!("none: {:?}", none);
+}
+
+fn value_in_cent(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
